172302cb53f873eacf54a772c9ac4fe422463e68
BlackPuma9 <yuliiayavorska9@gmail.com>
2022-11-13 18:06:38 +0100

    Modify JS file. Declare value in object for the first column

--- main.js
+++ main.js
@@ -8,6 +8,10 @@
         const qaEmail = document.getElementById('qa_mail');
         const agentEmail = document.getElementById('agent_mail');
         const qacomment = document.getElementById('qacomment');
+        const improvArea = document.getElementById('improveline');
+        const quater = document.getElementById('quater');
+        const week = document.getElementById('week');
+        const year = document.getElementById('year');
 
         // Below all the patterns
         const emailRegExp =/.+@scori\.com/;
@@ -18,7 +22,11 @@
           caseRegExp.test(number.value), 
           emailRegExp.test(qaEmail.value),
           emailRegExp.test(agentEmail.value),
-          qacomment.value
+          qacomment.value,
+          improvArea.value,
+          quater.value,
+          week.value,
+          year.value
         ]);
     });
   }
