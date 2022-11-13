92f0eb90048a9204610640af82b71b27ff13b07d
BlackPuma9 <yuliiayavorska9@gmail.com>
2022-11-13 19:05:20 +0100

    Created for each markown const

--- main.js
+++ main.js
@@ -12,6 +12,24 @@
         const quater = document.getElementById('quater');
         const week = document.getElementById('week');
         const year = document.getElementById('year');
+        const customerAut = document.querySelector('input[name = customer_authorization]:checked');
+        const greetTone = document.querySelector('input[name = greetings_tone]:checked');
+        const resolution = document.querySelector('input[name = case_resolution]:checked');
+        const policy = document.getElementById('policy');
+        const category = document.querySelector('input[name = case_category]:checked');
+        const resolutionTime = document.querySelector('input[name = resolution_time]:checked');
+        const transfer = document.querySelector('input[name = transfer]:checked');
+        const accuracyReply = document.querySelector('input[name = reply_accuracy]:checked');
+        const caseStatus = document.querySelector('input[name = case_status]:checked');
+        const techChat = document.querySelector('input[name = tech_chat]:checked');
+        const language = document.querySelector('input[name = language]:checked');
+
+
+
+
+        if (!customerAut||!greetTone||!resolution||!category||!resolutionTime||!transfer||!accuracyReply||!caseStatus||!techChat||!language) {
+          alert('Choose the markdowns');
+        }
 
         // Below all the patterns
         const emailRegExp =/.+@scori\.com/;
@@ -26,7 +44,18 @@
           improvArea.value,
           quater.value,
           week.value,
-          year.value
+          year.value,
+          customerAut.value,
+          greetTone.value,
+          resolution.value,
+          policy.value,
+          category.value, 
+          resolutionTime.value,
+          transfer.value,
+          accuracyReply.value,
+          caseStatus.value,
+          techChat.value,
+          language.value
         ]);
     });
   }
